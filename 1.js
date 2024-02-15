df12360aa1e3642fb45ed365cc1da524315cbb3e
richhomiequan93 <kc0903@hotmail.com>
2024-02-15 14:34:41 +0800

    Initial commit

---
+++ 1.js
@@ -0,0 +1,15 @@
+{
+    "name": "NFT #1",
+    "description": "This NFT represents the number 1 out of 88, part of Batch 1.",
+    "image": "URL to image representing NFT #1",
+    "attributes": [
+        {
+            "trait_type": "88/88",
+            "value": "1/88"
+        },
+        {
+            "trait_type": "Batch",
+            "value": "1"
+        }
+    ]
+}
